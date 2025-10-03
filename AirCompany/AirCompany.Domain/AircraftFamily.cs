f50bfc9510975a11e8fa5caa56ec938f3a9b178d
Marina <nozdryakovamarina94@gmail.com>
2025-10-03 22:29:40 +0400

    add entity classes for airline domain model feat: create AircraftFamily, AircraftModel, Flight, Passenger, Ticket

---
+++ AirCompany/AirCompany.Domain/AircraftFamily.cs
@@ -0,0 +1,8 @@
+﻿namespace AirCompany.Domain
+
+public class AircraftFamily
+{
+    public required int Id { get; set; }
+    public required string FamilyName { get; set; }
+    public required string Manufacturer {  get; set; }
+}
