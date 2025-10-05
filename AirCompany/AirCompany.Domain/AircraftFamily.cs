926bd8e2310e0bcbb5382f923d60b7a39cedd14f
Marina <nozdryakovamarina94@gmail.com>
2025-10-05 14:56:22 +0400

    docs: add XML documentation to domain entities

--- AirCompany/AirCompany.Domain/AircraftFamily.cs
+++ AirCompany/AirCompany.Domain/AircraftFamily.cs
@@ -1,8 +1,22 @@
 ﻿namespace AirCompany.Domain;
 
+/// <summary>
+/// Represents an <see cref="AircraftFamily"/>
+/// </summary>
 public class AircraftFamily
 {
+    /// <summary>
+    /// The unique identifier for the <see cref="AircraftFamily"/>
+    /// </summary>
     public required int Id { get; set; }
+
+    /// <summary>
+    /// Name of the <see cref="AircraftFamily"/>
+    /// </summary>
     public required string FamilyName { get; set; }
+
+    /// <summary>
+    /// Manufacturer of the <see cref="AircraftFamily"/>
+    /// </summary>
     public required string Manufacturer { get; set; }
 }
--- AirCompany/AirCompany.Domain/Ticket.cs
+++ AirCompany/AirCompany.Domain/Ticket.cs
@@ -1,11 +1,37 @@
 ﻿namespace AirCompany.Domain;
 
+/// <summary>
+/// Represents a <see cref="Ticket"/> issued to a <see cref="Passenger"/> for a specific <see cref="Flight"/>
+/// </summary>
 public class Ticket
 {
+    /// <summary>
+    /// The unique identifier for the <see cref="Ticket"/>
+    /// </summary>
     public required int Id { get; set; }
+
+    /// <summary>
+    /// <see cref="Flight"/> associated with this <see cref="Ticket"/>
+    /// </summary>
     public required Flight Flight { get; set; }
+
+    /// <summary>
+    /// <see cref="Passenger"/> who owns this <see cref="Ticket"/>
+    /// </summary>
     public required Passenger Passenger { get; set; }
+
+    /// <summary>
+    /// The seat number assigned to this <see cref="Ticket"/>
+    /// </summary>
     public required string SeatNumber { get; set; }
+
+    /// <summary>
+    /// Indicates whether the <see cref="Passenger"/> has hand luggage for this <see cref="Ticket"/>
+    /// </summary>
     public bool? HasHandLuggage { get; set; }
+
+    /// <summary>
+    /// Total baggage weight (in kilograms) for this <see cref="Ticket"/>
+    /// </summary>
     public double? TotalBaggageWeightKg { get; set; }
 }
